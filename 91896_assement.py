e86c8b15666c9f504c0df54a05cd2769325ed318
MohammadSaifAhmed <ma21487@my.westlake.school.nz>
2024-03-18 09:34:39 +1300

    testing document and tasks dictionary made

---
+++ 91896_assement.py
@@ -0,0 +1,44 @@
+tasks = {
+    "T1":{
+        "Title":"Design Homepage",
+        "Description":"Create a mockup of the homepage",
+        "Assignee":"JSM",
+        "Priority":3,
+        "Status":"In Progress"
+
+    },
+    "T2":{
+        "Title":"Implement Login page",
+        "Description":"",
+        "Assignee":"",
+        "Priority":1,
+        "Status":""
+
+    },
+    "T3":{
+        "Title":"",
+        "Description":"",
+        "Assignee":"",
+        "Priority":1,
+        "Status":""
+
+    },
+    "T4":{
+        "Title":"",
+        "Description":"",
+        "Assignee":"",
+        "Priority":1,
+        "Status":""
+
+    },
+    "T5":{
+        "Title":"",
+        "Description":"",
+        "Assignee":"",
+        "Priority":1,
+        "Status":""
+
+    },
+
+
+}
