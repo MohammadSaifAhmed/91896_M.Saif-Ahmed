a99e64c425654554fbfbd60ac5c2b27beea2ef57
MohammadSaifAhmed <ma21487@my.westlake.school.nz>
2024-03-19 15:10:18 +1300

    new task fuct and team members dict

--- 91896_assement.py
+++ 91896_assement.py
@@ -1,3 +1,5 @@
+from easygui import *
+
 tasks = {
     "T1":{
         "Title":"Design Homepage",
@@ -9,36 +11,107 @@ tasks = {
     },
     "T2":{
         "Title":"Implement Login page",
-        "Description":"",
-        "Assignee":"",
-        "Priority":1,
-        "Status":""
+        "Description":"Create the Login page for the website",
+        "Assignee":"JSM",
+        "Priority":3,
+        "Status":"Blocked"
 
     },
     "T3":{
-        "Title":"",
-        "Description":"",
-        "Assignee":"",
+        "Title":"Fix navigation menu",
+        "Description":"Fix the navigation menu to be more user-friendly",
+        "Assignee":"None",
         "Priority":1,
-        "Status":""
+        "Status":"Not Started"
 
     },
     "T4":{
-        "Title":"",
-        "Description":"",
-        "Assignee":"",
-        "Priority":1,
-        "Status":""
+        "Title":"Add payment processing",
+        "Description":"Implement payment processing for the website",
+        "Assignee":"JLO",
+        "Priority":2,
+        "Status":"In Progress"
 
     },
     "T5":{
-        "Title":"",
-        "Description":"",
-        "Assignee":"",
+        "Title":"Create an About Us page",
+        "Description":"Create a page with information about the company",
+        "Assignee":"BDI",
         "Priority":1,
-        "Status":""
+        "Status":"Blocked"
 
     },
-
-
 }
+
+team_members = {
+    "JSM":{
+        "Name":"John Smith",
+        "Email":"John@techvision.com",
+        "Tasks Assigned":["T1","T2"],
+    },
+    "JLO":{
+        "Name":"Jane Love",
+        "Email":"Jane@techvision.com",
+        "Tasks Assigned":["T4"],
+    },
+    "BDI":{
+        "Name":"Bob Dillon",
+        "Email":"Bob@techvision.com",
+        "Tasks Assigned":["T5"],
+    },
+}
+
+
+def display():
+    pass
+
+def new_task():
+    input_list = tasks["T1"].keys()
+    print(input_list)
+
+def update_task():
+    pass
+
+def search():
+    pass
+
+def report():
+    pass
+
+def task_collection():
+    pass
+
+def leave():
+    pass
+
+options = {
+        "New Task":new_task,
+        "search employee or task":search,
+        "Update task":update_task,
+        "Generate report":report,
+        "see task collection":task_collection,
+        "Exit":leave,
+     
+        
+    }
+
+
+running = True
+while running:
+    
+    msg = "What would you like to do?"
+    title = "Task Management System"
+
+    choices = (list(options.keys()))
+    
+
+    selection = buttonbox(msg,title,choices)
+    
+    if not options.get(selection):
+        running = False
+    else:
+        user_choice = options[selection]()
+
+
+        if not user_choice:
+            running = False
